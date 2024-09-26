2fb951ccdb0d000fd352bfd9c018065e67bc0ad0
Arjun Sharma <120898184+arjunsharma999@users.noreply.github.com>
2024-09-26 15:39:59 +0530

    changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,6 +7,30 @@ function Footer() {
     <div className='foot-txt'>
         For more query !!
         <a href="https://www.linkedin.com/in/arjun-sharma-62509925b/" target='-'> LinkdLn</a>
+
+        <div className='social-media'>
+                    <ul className='socipal-media-deskto'>
+                        <li> <a href="https://www.instagram.com/arjun_sharma99/"> <ion-icon name="logo-instagram"></ion-icon></a></li>
+
+
+                        <li> <a href="https://github.com/arjunsharma999"> <ion-icon name="logo-github"></ion-icon> </a></li>
+
+                        <li>
+                            <a href="https://www.linkedin.com/in/arjun-sharma-62509925b/">
+                                <ion-icon name="link-outline"></ion-icon>
+                            </a> </li>
+                        <li>
+                            <a href="">
+                                <ion-icon name="logo-facebook"></ion-icon></a>
+                        </li>
+
+
+
+                    </ul>
+
+                    
+
+                </div>    
         
     </div>
 
